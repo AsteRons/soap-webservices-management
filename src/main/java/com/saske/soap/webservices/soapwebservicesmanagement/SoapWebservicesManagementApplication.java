05cd9b52a2707b7aa1b76457bfbe7e666edd0b7e
Maciej Rudnik <maciej.rudnik2@gmail.com>
2021-06-21 14:26:52 +0200

    init webservice project

---
+++ src/main/java/com/saske/soap/webservices/soapwebservicesmanagement/SoapWebservicesManagementApplication.java
@@ -0,0 +1,13 @@
+package com.saske.soap.webservices.soapwebservicesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapWebservicesManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapWebservicesManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saske/soap/webservices/soapwebservicesmanagement/SoapWebservicesManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saske.soap.webservices.soapwebservicesmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapWebservicesManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
