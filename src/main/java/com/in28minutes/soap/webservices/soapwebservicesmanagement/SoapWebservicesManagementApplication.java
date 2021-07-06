ade18962bc39f15158d0b25750ba2c47c9bce539
Maciej Rudnik <maciej.rudnik2@gmail.com>
2021-07-06 15:50:52 +0200

    Introduction to Java API for XML Binding (JAXB) and Configuring JAXB 2

--- src/main/java/com/in28minutes/soap/webservices/soapwebservicesmanagement/SoapWebservicesManagementApplication.java
+++ src/main/java/com/in28minutes/soap/webservices/soapwebservicesmanagement/SoapWebservicesManagementApplication.java
@@ -1,4 +1,4 @@
-package com.saske.soap.webservices.soapwebservicesmanagement;
+package com.in28minutes.soap.webservices.soapwebservicesmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/in28minutes/soap/webservices/soapwebservicesmanagement/SoapWebservicesManagementApplicationTests.java
+++ src/test/java/com/in28minutes/soap/webservices/soapwebservicesmanagement/SoapWebservicesManagementApplicationTests.java
@@ -1,4 +1,4 @@
-package com.saske.soap.webservices.soapwebservicesmanagement;
+package com.in28minutes.soap.webservices.soapwebservicesmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
